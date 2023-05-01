6931afed01fcca9eedff6c7a1b1f99b26c14d560
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-05-01 16:26:30 +0100

    Check database in health endpoint

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,11 +1,11 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use {
+    crate::AppState,
+    axum::{extract::State, http::StatusCode, response::IntoResponse},
+};
 
 /// Tests node health
-pub async fn health() -> impl IntoResponse {
-    // todo fix this
-    let can_fetch_from_saint_sport = true;
-
-    if can_fetch_from_saint_sport {
+pub async fn health(State(AppState { db, .. }): State<AppState>) -> impl IntoResponse {
+    if sqlx::query("SELECT 1").fetch_one(&db).await.is_ok() {
         StatusCode::OK
     } else {
         StatusCode::SERVICE_UNAVAILABLE
